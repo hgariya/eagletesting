a1d2511abe9ac82d3ee603fcd370397d1485c8c4
nitin-pulp <nitin@pulpstrategy.com>
2016-11-17 15:09:19 +0530

    eee

---
+++ hh.php
@@ -0,0 +1,54 @@
+<div class="brdcrm">
+       <div class="big-cont mt0">
+        <div class="crm">
+           <a href="index.php">Home</a><span> >> </span><a href="#" class="active-crum">Success Stories</a>
+           </div>
+           </div>
+
+       </div>
+
+<div class="big-cont mt0 suc">
+   <!--<ul class="pagi fr" >
+        <li><a href="#"><i class="fa fa- fa-angle-double-left"></i></a></li>
+        <li><a href="#">1</a></li>
+        <li><a href="#">1</a></li>
+        <li><a href="#">1</a></li>
+        <li><a href="#"><i class="fa  fa-angle-double-right"></i></a></li>
+    </ul>-->
+
+    <div class="suc-st">
+        <span>Title of story</span>
+        <div class="box"></div>
+        <p class="tit-hd">Description</p>
+        <p class="tit-desc">Nemo architecto voluptate dolorem. doloremque quod ipsam voluptas dolor et rerum nam hic delectus ut vel amet. itaque quisquam aut in aut quis doloribus amet nisi et rem. qui voluptatibus iusto veniam quidem voluptas deleniti quaerat occaecati ipsa cum ipsa in vero. et nihil ipsa facilis et sed aut nesciunt ea in architecto possimus ut iusto et eos odit iste consectetur hic quasi sunt. </p>
+        <div class="tit-down">
+                <a href="#"><img src="images/ios-store.png" alt=""/></a>
+                <a href="#"><img src="images/play-icon.png" alt=""/></a>
+        </div>
+
+    </div>
+
+    <div class="suc-st">
+        <span>Title of story</span>
+        <div class="box"></div>
+        <p class="tit-hd">Description</p>
+        <p class="tit-desc">Nemo architecto voluptate dolorem. doloremque quod ipsam voluptas dolor et rerum nam hic delectus ut vel amet. itaque quisquam aut in aut quis doloribus amet nisi et rem. qui voluptatibus iusto veniam quidem voluptas deleniti quaerat occaecati ipsa cum ipsa in vero. et nihil ipsa facilis et sed aut nesciunt ea in architecto possimus ut iusto et eos odit iste consectetur hic quasi sunt. </p>
+        <div class="tit-down">
+                <a href="#"><img src="images/ios-store.png" alt=""/></a>
+                <a href="#"><img src="images/play-icon.png" alt=""/></a>
+        </div>
+
+    </div>
+
+    <div class="suc-st">
+        <span>Title of story</span>
+        <div class="box"></div>
+        <p class="tit-hd">Description</p>
+        <p class="tit-desc">Nemo architecto voluptate dolorem. doloremque quod ipsam voluptas dolor et rerum nam hic delectus ut vel amet. itaque quisquam aut in aut quis doloribus amet nisi et rem. qui voluptatibus iusto veniam quidem voluptas deleniti quaerat occaecati ipsa cum ipsa in vero. et nihil ipsa facilis et sed aut nesciunt ea in architecto possimus ut iusto et eos odit iste consectetur hic quasi sunt. </p>
+        <div class="tit-down">
+                <a href="#"><img src="images/ios-store.png" alt=""/></a>
+                <a href="#"><img src="images/play-icon.png" alt=""/></a>
+        </div>
+
+    </div>
+</div>
